46b7c5c864fbe7949e9d6ec51ec79e8e1fa76ce5
jasonnguyen <neilgroup101@gmail.com>
2025-10-21 20:18:48 -0400

    fixing CORS origins and adding torch to requirements

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/SP-14-Green-Chess-AI/', // Matches your deployment path
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Default output directory
-    assetsDir: 'assets', // Default assets folder
-  },
+  base: "/SP-14-Green-Chess-AI/", // MUST match repo name
 });
